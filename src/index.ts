b57912787cddb5e83e6149d9865b16a032c164c6
justKevv <kevinbramastaa@gmail.com>
2026-05-04 19:23:15 +0800

    fix: change binding from simple types to using cloudflrae configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Hono } from 'hono'
-import { Bindings } from './types/kv';
 import { shorten } from './routes/shorten';
 import { redirect } from './routes/redirect';
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', async (c) => {
   const allKeys = await c.env.JUSTALINK.list();
--- src/types/kv.ts
+++
@@ -1,5 +0,0 @@
-import type { KVNamespace } from '@cloudflare/workers-types'
-
-export type Bindings = {
-  JUSTALINK: KVNamespace
-}
